146c7877e5c9cd7101ee0b89fde0b80be1b1c591
Matt Enyeart <menyeart1@gmail.com>
2022-11-01 19:09:13 -0600

    Add the answers to the comparison assignment

--- comparisons.rb
+++ comparisons.rb
@@ -22,24 +22,31 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 # this should print: "Is numberTeachers greater than numberStudents?" false
 
 # YOU DO: print the result of the comparison: is number_teachers less than number_students?
+print number_teachers < number_students
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_teachers equal to string_teachers?
+print number_teachers == string_teachers
 # this should print: false
 
 # YOU DO: print the result of the comparison: is number_teachers not equal to number_students?
+print number_teachers != number_students
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 20?
+print number_students >= 20
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 21?
+print number_students >= 21
 # this should print: false
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 20?
+print number_students <= 20
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 21?
+print number_students <= 21
 # this should print: true
 
 
@@ -54,20 +61,24 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 
 puts 4 < 9
 #YOU DO: Explain.
+#Display whether number 4 is smaller than number nine evaluates to true or false. The comparison will evaluate to true.
 
 books = 3
 puts 4 < books
 # YOU DO: Explain.
+# The label books has been assigned to the value of the number 3. Display whether number 4 is smaller than the variable books evaluates to true or false. The comparison will evaluate to false.
 
 friends = 6
 siblings = 2
 puts friends > siblings
 # YOU DO: Explain.
+#The label friends has been assigned to the value of the number 3. The label siblings has been assigned to the value of the number 2. Display whether the value represented by friends is greater than the value represented by the variable siblings evaluates to true or false.
 
 attendees = 9
 meals = 8
 puts attendees != meals
 # YOU DO: Explain.
+#The label attendees is assigned to the value of the number 9. The label meals has been assigned to the value of the number 8. Display whether the variable attendees does not equal the variable meals is true or false. 
 
 
 #-------------------
@@ -90,15 +101,16 @@ age = 1
 
 # YOU DO:
 # Determine if the dog loves to play and loves treats
-
+puts loves_to_play && loves_treats
 
 # Determine if the dog loves to play and loves the dog park
-
+puts loves_to_play && loves_dog_park
 
 # Determine if the dog loves to play or loves the dog park
-
+puts loves_to_play || loves_dog_park
 
 # Determine if the dog loves to play and is a puppy
+puts loves_to_play && age <= 1
 
 # What did your final line of code evaluate to? Why do you think that is? Explain.
-# ANSWER:
+# ANSWER: I decided that to be a puppy a dog's age would have to be less than or equal to 1 year old. I didn't see a variable for being a puppy so I don't think this could be evaluated.
